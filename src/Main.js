56aa8a7050599a270d74687ce2baa037ccbcb6d6
Chase <chasewashley@gmail.com>
2020-07-01 09:36:28 -0400

    minor change

--- src/Main.js
+++ src/Main.js
@@ -30,7 +30,7 @@ class Main extends React.Component {
       descendant: '',
       lastDescendant: '',
       category: null,
-      lastCategory: '',
+      lastCategory: null,
       locationText: '',
       lastLocationText: '',
       categoryText: '',
